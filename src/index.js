c6b1770beb20234bf4842e4468033696ce972ecb
Filipe Paulino <filipepaulinodeveloper@hotmail.com>
2023-07-06 10:12:53 -0300

    classe perosangem criada

--- src/index.js
+++ src/index.js
@@ -1,33 +1,4 @@
-// const personagemPedrinho = {
-//     nome: 'Pedrinho',
-//     vida: 7,
-//     mana: 12,
-//     level: 5,
-//     tipo: 'Mago',
-// }
+import { Personagem } from "./modules/personagem.js";
 
-const personagemJose = {
-    nome: 'Jose',
-    vida: 7,
-    mana: 6,
-    level: 3,
-    tipo: 'Arqueiro',
-}
-
-class Personagem {
-    nome
-    vida
-    mana
-    viada
-    level
-    tipo
-    descricao 
-}
-
-const personagemPedrinho = new Personagem();
-
-personagemPedrinho.nome = 'Pedrinho'
-personagemPedrinho.vida = 7
-personagemPedrinho.mana = 12
-personagemPedrinho.level = 5
-personagemPedrinho.tipo = 'Mago'
+const personagemPedrinho = new Personagem('Pedrinho', 7, 'Mago' );
+const personagemJose = new Personagem('Jose', 3, 'Arqueiro' );
